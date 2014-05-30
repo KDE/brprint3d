5b5070b6b2c449f142e85fd521b68da5f68294e4
Lays Rodrigues <laysrodriguessilva@gmail.com>
2014-05-30 10:05:36 -0300

    Start to create models
    First model create to manage the bed of the 3d printer
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

---
+++ qt-models/bedmodel.cpp
@@ -0,0 +1,6 @@
+#include "bedmodel.h"
+
+bedModel::bedModel()
+{
+
+}
---
+++ qt-models/bedmodel.h
@@ -0,0 +1,11 @@
+#ifndef BEDMODEL_H
+#define BEDMODEL_H
+
+
+class bedModel : public QAbstractListModel
+{
+public:
+    bedModel();
+};
+
+#endif // BEDMODEL_H
