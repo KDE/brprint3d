ebcaba768f4718a14eecfd09004d5142c01b8cb5
Lays Rodrigues <laysrodriguessilva@gmail.com>
2014-05-30 10:11:21 -0300

    Overrite Methods
    Declare default overrite methods on header and add the declarations
    on source.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- qt-models/bedmodel.cpp
+++ qt-models/bedmodel.cpp
@@ -4,3 +4,23 @@ bedModel::bedModel()
 {
 
 }
+
+int bedModel::rowCount(const QModelIndex &parent) const
+{
+
+}
+
+QVariant bedModel::data(const QModelIndex &index, int role) const
+{
+
+}
+
+bool bedModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+
+}
+
+Qt::ItemFlags bedModel::flags(const QModelIndex &index) const
+{
+
+}
--- qt-models/bedmodel.h
+++ qt-models/bedmodel.h
@@ -1,11 +1,19 @@
 #ifndef BEDMODEL_H
 #define BEDMODEL_H
 
+#include <QAbstractListModel>
 
 class bedModel : public QAbstractListModel
 {
+    Q_OBJECT
 public:
     bedModel();
+
+private:
+    virtual int rowCount(const QModelIndex &parent) const;
+    virtual QVariant data(const QModelIndex &index, int role) const;
+    virtual bool setData(const QModelIndex &index, const QVariant &value, int role);
+    virtual Qt::ItemFlags flags(const QModelIndex &index) const;
 };
 
 #endif // BEDMODEL_H
