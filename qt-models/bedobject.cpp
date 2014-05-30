460874d5eecb9efdd95cf76aecfcd93183de5755
Lays Rodrigues <laysrodriguessilva@gmail.com>
2014-05-30 11:02:05 -0300

    New Methods
    Create the get/set methods for each variable, improving
    the ideia of encapsuling data.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- qt-models/bedobject.cpp
+++ qt-models/bedobject.cpp
@@ -1,6 +1,43 @@
 #include "bedobject.h"
 
-bedObject::bedObject()
+bedObject::bedObject(int x, int y, int z)
 {
+    areaX = x;
+    areaY = y;
+    areaZ = z;
+}
 
+int bedObject::getX()
+{
+    return areaX;
+}
+
+int bedObject::getY()
+{
+    return areaY;
+}
+
+int bedObject::getZ()
+{
+    return areaZ;
+}
+
+double bedObject::getDesireTemp()
+{
+    return desireTemp;
+}
+
+double bedObject::getCurrTemp()
+{
+    return currTemp;
+}
+
+void bedObject::setDesireTemp(double t)
+{
+    desireTemp = t;
+}
+
+void bedObject::setCurrTemp(double t)
+{
+    currTemp = t;
 }
--- qt-models/bedobject.h
+++ qt-models/bedobject.h
@@ -5,7 +5,23 @@
 class bedObject
 {
 public:
-    bedObject();
+    bedObject(int x, int y, int z);
+
+    int getX();
+    int getY();
+    int getZ();
+    double getDesireTemp();
+    double getCurrTemp();
+    void setDesireTemp(double t);
+    void setCurrTemp(double t);
+
+private:
+
+    double currTemp;
+    double desireTemp;
+    int areaX;
+    int areaY;
+    int areaZ;
 };
 
 #endif // BEDOBJECT_H
