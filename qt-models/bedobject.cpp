122fe71110136fcebc5e7cb041a51167d926c749
Lays Rodrigues <laysrodriguessilva@gmail.com>
2014-05-30 11:00:24 -0300

    New Class to manage the bed data
    I realize that is better have a separeted class to manage
    the bed object.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

---
+++ qt-models/bedobject.cpp
@@ -0,0 +1,6 @@
+#include "bedobject.h"
+
+bedObject::bedObject()
+{
+
+}
---
+++ qt-models/bedobject.h
@@ -0,0 +1,11 @@
+#ifndef BEDOBJECT_H
+#define BEDOBJECT_H
+
+
+class bedObject
+{
+public:
+    bedObject();
+};
+
+#endif // BEDOBJECT_H
