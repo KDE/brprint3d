527269df86b716e2160caf6f67d021a98076f3b8
Tomaz Canabrava <tomaz.canabrava@intel.com>
2015-11-10 17:45:49 -0200

    Added License to files and headers
    Signed-off-by: Tomaz Canabrava <tomaz.canabrava@intel.com>

--- arduinolistener.cpp
+++ arduinolistener.cpp
@@ -1,8 +1,26 @@
-/*
- * This file starts a thread that run while the arduino is not connect or find
- *
- *
-*/
+/*=====================================================================
+
+ BRPrint3D, Open Source 3D Printing Host
+
+ (c) 2015 BRPrint3D Authors 
+
+ This file is part of the BRPrint3D project
+
+ BRPrint3D is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ BRPrint3D is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with BRPrint3D. If not, see <http://www.gnu.org/licenses/>.
+
+ ======================================================================*/
+
 #include "arduinolistener.h"
 
 arduinoListener::arduinoListener()
--- brprint3d.cpp
+++ brprint3d.cpp
@@ -1,18 +1,38 @@
+/*=====================================================================
+
+ BRPrint3D, Open Source 3D Printing Host
+
+ (c) 2015 BRPrint3D Authors 
+
+ This file is part of the BRPrint3D project
+
+ BRPrint3D is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ BRPrint3D is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with BRPrint3D. If not, see <http://www.gnu.org/licenses/>.
+
+ ======================================================================*/
+
 #include "brprint3d.h"
 #include "ui_Pandora.h"
-
 
 BrPrint3D::BrPrint3D(QWidget *parent) : QMainWindow(parent),
 ui(new Ui::BrPrint3D)
 {
     ui->setupUi(this);
-
 }
 
 BrPrint3D::~BrPrint3D()
 {
     delete ui;
-
 }
 
 QSettings settings(QDir::currentPath()+"/br.ini",QSettings::IniFormat); //INI File
