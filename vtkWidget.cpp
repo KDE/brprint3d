dfc650aa039161c58d0aa3b38372f77819106784
Lays Rodrigues <laysrodriguessilva@gmail.com>
2015-11-12 23:10:00 -0200

    Made Ajusts
    Made some ajusts in code. Not very specific.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- vtkWidget.cpp
+++ vtkWidget.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * This class has a deprecated library!
  * QVTKWidget is disused, so this class will not be used for long.
  * Only I'm working with her because I'm learning VTK and many Qt + VTK tutorials still use this library,
@@ -7,11 +7,32 @@
  * Lays Rodrigues November 7,2015
  * */
 
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 #include "vtkWidget.h"
 
 vtkWidget::vtkWidget()
 {
+    
+}
 
+vtkWidget::~vtkWidget()
+{
+    
 }
 void vtkWidget::renderSTL(QString pathStl)
 {
--- vtkWidget.h
+++ vtkWidget.h
@@ -1,5 +1,20 @@
 /*
- * This class has a deprecated library!
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+/*
+ * This class use a deprecated library!
  * QVTKWidget is disused, so this class will not be used for long.
  * Only I'm working with her because I'm learning VTK and many Qt + VTK tutorials still use this library,
  * with time I will be carrying to the new class.
@@ -8,6 +23,7 @@
  * */
 #ifndef VTKWIDGET_H
 #define VTKWIDGET_H
+
 #include <QVTKWidget.h>
 #include <vtkPolyData.h>
 #include <vtkSTLReader.h>
@@ -26,10 +42,15 @@
 
 class vtkWidget : public QVTKWidget
 {
+    Q_OBJECT
 public:
-    vtkWidget();
+vtkWidget();
+~vtkWidget();
     void renderSTL(QString path);
     void renderGcode(QString text);
+
+private:
+
 };
 
-#endif // VTKWIDGET2_H
+#endif // VTKWIDGET_H
