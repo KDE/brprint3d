f2d7573fea4be6710a58bfb5e5353a808a5a0456
Lays Rodrigues <laysrodriguessilva@gmail.com>
2015-11-17 00:43:15 -0200

    Redone Class
    Redone class because i was having some issues on the UI
    and cpp files.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- ExtruderControlWidget.h
+++ ExtruderControlWidget.h
@@ -1,45 +1,22 @@
-/*
- * <one line to give the library's name and an idea of what it does.>
- * Copyright (C) 2015  <copyright holder> <email>
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- */
-
 #ifndef EXTRUDERCONTROLWIDGET_H
 #define EXTRUDERCONTROLWIDGET_H
 
-#include <qt5/QtWidgets/QWidget>
-#include <stdio.h>
+#include <QWidget>
 
-namespace Ui
-{
+namespace Ui {
 class ExtruderControlWidget;
 }
 
 class ExtruderControlWidget : public QWidget
 {
     Q_OBJECT
+
 public:
-explicit ExtruderControlWidget(QWidget *parent = 0);
-~ExtruderControlWidget();
-void init();
+    explicit ExtruderControlWidget(QWidget *parent = 0);
+    ~ExtruderControlWidget();
 
 private:
-    Ui::ExtruderControlWidget* ui;
-private slots:
-    void updatePos(double posX, double posY, double posZ);
+    Ui::ExtruderControlWidget *ui;
 };
 
 #endif // EXTRUDERCONTROLWIDGET_H
