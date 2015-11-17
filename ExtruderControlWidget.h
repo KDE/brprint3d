1df2758029611d7b224ec6a4f773655caeeaa5c1
Lays Rodrigues <laysrodriguessilva@gmail.com>
2015-11-17 00:44:38 -0200

    Redone Class
    Redone the class to solve issues with the Ui file.
    Add new functions:
    -Set Extruders to home position
    -Set Extruders to new positions
    -Get PrinterObject to control the 3DPrint
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- ExtruderControlWidget.h
+++ ExtruderControlWidget.h
@@ -1,7 +1,26 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright (C) 2015  <copyright holder> <email>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 #ifndef EXTRUDERCONTROLWIDGET_H
 #define EXTRUDERCONTROLWIDGET_H
 
 #include <QWidget>
+#include "Repetier.h"
 
 namespace Ui {
 class ExtruderControlWidget;
@@ -15,8 +34,21 @@ public:
     explicit ExtruderControlWidget(QWidget *parent = 0);
     ~ExtruderControlWidget();
 
-private:
-    Ui::ExtruderControlWidget *ui;
+    void init();
+        void getPrinterObject(Repetier *pObject);
+
+    private:
+        Ui::ExtruderControlWidget* ui;
+        Repetier *printerObject;
+    private slots:
+        void updatePos(double posX, double posY, double posZ);
+        void setHomeX();
+        void setHomeY();
+        void setHomeZ();
+        void setHomeXYZ();
+        void setPosX();
+        void setPosY();
+        void setPosZ();
 };
 
 #endif // EXTRUDERCONTROLWIDGET_H
