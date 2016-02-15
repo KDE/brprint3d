fed25e382381d7cad91d2624cff80f1946a105be
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-02-15 16:33:57 -0200

    Create some default methods
    Creathe the default methods to a QAbstractTableModel, also
    add some data to tests.
    This class isnt by all means finish.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- extrudermodel.cpp
+++ extrudermodel.cpp
@@ -1,6 +1,90 @@
 #include "extrudermodel.h"
 
+struct extruderObject{
+ int index;
+ double temperature;
+ int retract;
+};
+
 extruderModel::extruderModel()
 {
+}
 
+void extruderModel::setPrinterObjetc(Repetier *p)
+{
+    printerObject = p;
+    for(int i=0;i<printerObject->getNoOfExtruders();i++){
+    }
+}
+
+int extruderModel::rowCount(const QModelIndex &parent) const
+{   Q_UNUSED(parent);
+    return 4;
+}
+
+int extruderModel::columnCount(const QModelIndex &parent) const
+{
+    return 3;//printerObject->getNoOfExtruders();
+}
+
+QVariant extruderModel::data(const QModelIndex &index, int role) const
+{
+   // double *temperatures = printerObject->getAllExtrudersTemp();
+    if(Qt::DisplayRole != role)
+        return QVariant();
+    else{
+
+        switch (index.column()) {
+        case 0:{
+            return 10;//temperatures[index.row()];
+        }break;
+        case 1:{
+            //Cooler Fan
+            return 20;
+        }break;
+        case 2:{
+            //Extruder/Retract
+            return 30;
+        }break;
+
+        default:
+            break;
+        }
+    }
+}
+
+bool extruderModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+}
+
+QVariant extruderModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role == Qt::DisplayRole)
+       {
+           if (orientation == Qt::Horizontal) {
+               switch (section)
+               {
+               case 0:
+                   return QString("Temperature");
+               case 1:
+                   return QString("Cooler Fan");
+               case 2:
+                   return QString("Extruder/Retract");
+               }
+           }
+           if(orientation == Qt::Vertical){
+               switch (section) {
+               case 0:
+                  return QString("Extruder One");
+                   break;
+               default:
+                   break;
+               }
+           }
+       }
+    return QVariant();
+}
+
+Qt::ItemFlags extruderModel::flags(const QModelIndex &index) const
+{
 }
--- extrudermodel.h
+++ extrudermodel.h
@@ -1,11 +1,28 @@
 #ifndef EXTRUDERMODEL_H
 #define EXTRUDERMODEL_H
-
+#include <QAbstractTableModel>
+#include "KI/Repetier.h"
+#include <stdio.h>
+#include <QTimer>
 
 class extruderModel : public QAbstractTableModel
 {
 public:
     extruderModel();
+    void setPrinterObjetc(Repetier *p);
+private:
+    int m_rowCount;
+    int m_columnCount;
+    virtual int rowCount(const QModelIndex &parent) const;
+    virtual int columnCount(const QModelIndex &parent) const;
+    virtual QVariant data(const QModelIndex &index, int role) const;
+    virtual bool setData(const QModelIndex &index, const QVariant &value, int role);
+    Repetier *printerObject;
+    virtual QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    virtual Qt::ItemFlags flags(const QModelIndex &index) const;
+    QTimer timer;
+    struct extruderObject;
+    QList<extruderObject*> extruderList;
 };
 
 #endif // EXTRUDERMODEL_H
