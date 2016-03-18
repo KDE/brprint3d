7a978153519f69dc51c02dd9923cf41a909415ca
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-03-18 11:24:26 -0300

    Remove unnecessary code
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- brprint3d.cpp
+++ brprint3d.cpp
@@ -1,19 +1,7 @@
 #include "brprint3d.h"
 
-#include <QtGui/QLabel>
-#include <QtGui/QMenu>
-#include <QtGui/QMenuBar>
-#include <QtGui/QAction>
-
 brprint3d::brprint3d()
 {
-    QLabel* label = new QLabel( this );
-    label->setText( "Hello World!" );
-    setCentralWidget( label );
-    QAction* action = new QAction(this);
-    action->setText( "Quit" );
-    connect(action, SIGNAL(triggered()), SLOT(close()) );
-    menuBar()->addMenu( "File" )->addAction( action );
 }
 
 brprint3d::~brprint3d()
