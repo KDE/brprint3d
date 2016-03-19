8bb3cf1ec09d9a985bf326f1cb48514dccaa6834
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-03-19 19:02:27 -0300

    Override methods
    Include the override methods and the code for
    each one.
    Also include the method to update the temperature.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- qt-models/extrudermodel.cpp
+++ qt-models/extrudermodel.cpp
@@ -1,6 +1,71 @@
 #include "extrudermodel.h"
 
-ExtruderModel::ExtruderModel()
+ExtruderModel::ExtruderModel(int qntExtruders, QList<ExtruderObject> list)
 {
+    m_qntExtruders = qntExtruders;
+    extruderList = list;
+}
+
+int ExtruderModel::rowCount(const QModelIndex &parent) const
+{   Q_UNUSED(parent);
+    return 6;
+}
+
+int ExtruderModel::columnCount(const QModelIndex &parent) const
+{   Q_UNUSED(parent);
+    return m_qntExtruders;
+}
+
+QVariant ExtruderModel::data(const QModelIndex &index, int role) const
+{
+    if(Qt::DisplayRole != role)
+        return QVariant();
+    else{
+        //Maybe use pointer here???
+        ExtruderObject tmp = extruderList.at(index.row());
+        switch (index.column()){
+            case 0: return tmp.currTemp;
+            case 1: return tmp.getMaxTemp();
+            case 2: return tmp.getExtrusionSpeed();
+            case 3: return tmp.getFanSpeed();
+            case 4: return tmp.getMovSpeed();
+        }
+    }
+}
+
+bool ExtruderModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{   //Maybe use pointer??
+    ExtruderObject tmp = extruderList.at(index.row());
+    if (role == Qt::EditRole) {
+        switch(index.column()) {
+            case 1:  tmp.setMaxTemp(value.toInt()); emit dataChanged(index,index);
+            case 2:  tmp.setExtrusionSpeed(value.toInt()); emit dataChanged(index,index);
+            case 3:  tmp.setFanSpeed(value.toInt()); emit dataChanged(index,index);
+            case 4:  tmp.setMovSpeed(value.toInt()); emit dataChanged(index,index);
+         }
+    }else if(role == Qt::DisplayRole){
+        if(index.column() == 0){
+            tmp.currTemp = value.toDouble();
+            emit dataChanged(index,index);
+        }
+    }
+    return true;//Right place?
+}
+
+Qt::ItemFlags ExtruderModel::flags(const QModelIndex &index) const
+{   if(index.column()!=0)
+        return QAbstractTableModel::flags(index) | Qt::ItemIsEditable;
+    else
+        return QAbstractTableModel::flags(index);
+}
+
+void ExtruderModel::updateExtruderTemp()
+{   /* Use rand to sort random temps to test, but when the project is more advanced substitute this
+      for the info get from the printer */
+    std::srand (time(NULL));
+    for(int i=0; i < rowCount(QModelIndex()) ; i++){
+        const QVariant v(std::rand()%100);
+        setData(index(i,0),v ,Qt::DisplayRole); //Check this flag
+    }
 
 }
--- qt-models/extrudermodel.h
+++ qt-models/extrudermodel.h
@@ -1,11 +1,24 @@
 #ifndef EXTRUDERMODEL_H
 #define EXTRUDERMODEL_H
-
+//QtIncludes
+#include <QtCore/QAbstractTableModel>
+#include <QtCore/QTimer>
+//LocalIncludes
+#include "extruderobject.h"
 
 class ExtruderModel : public QAbstractTableModel
 {
 public:
-    ExtruderModel();
+    ExtruderModel(int qntExtruders, QList<ExtruderObject> list);
+    virtual int rowCount(const QModelIndex &parent) const;
+    virtual int columnCount(const QModelIndex &parent) const;
+    virtual QVariant data(const QModelIndex &index, int role) const;
+    virtual bool setData(const QModelIndex &index, const QVariant &value, int role);
+    virtual Qt::ItemFlags flags(const QModelIndex &index) const;
+private:
+    int m_qntExtruders;
+    QList <ExtruderObject> extruderList;
+    void updateExtruderTemp();
 };
 
 #endif // EXTRUDERMODEL_H
