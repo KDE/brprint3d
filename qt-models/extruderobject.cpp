8c81afb82172d84499fccc7e0c3b56bcbaca04c9
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-03-19 18:25:20 -0300

    Include get/sets
    Include the varibles to the extruder, and the get/set's
    necessary.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- qt-models/extruderobject.cpp
+++ qt-models/extruderobject.cpp
@@ -1,6 +1,50 @@
 #include "extruderobject.h"
 
-ExtruderObject::ExtruderObject()
+ExtruderObject::ExtruderObject(int nExtruder ,int maxtemp, int mov, int ext, int fan)
 {
+    extruderNumber = nExtruder;
+    maxTemp = maxtemp;
+    movimentSpeed = mov;
+    extrusionSpeed = ext;
+    fanSpeed = fan;
+}
 
+void ExtruderObject::setMaxTemp(int t)
+{
+    maxTemp = t;
+}
+
+void ExtruderObject::setMovSpeed(int s)
+{
+    movimentSpeed = s;
+}
+
+void ExtruderObject::setExtrusionSpeed(int s)
+{
+    extrusionSpeed = s;
+}
+
+void ExtruderObject::setFanSpeed(int s)
+{
+    fanSpeed = s;
+}
+
+int ExtruderObject::getMaxTemp()
+{
+    return maxTemp;
+}
+
+int ExtruderObject::getMovSpeed()
+{
+    return movimentSpeed;
+}
+
+int ExtruderObject::getExtrusionSpeed()
+{
+    return extrusionSpeed;
+}
+
+int ExtruderObject::getFanSpeed()
+{
+    return fanSpeed;
 }
--- qt-models/extruderobject.h
+++ qt-models/extruderobject.h
@@ -5,7 +5,23 @@
 class ExtruderObject
 {
 public:
-    ExtruderObject();
+    ExtruderObject(int nExtruder, int maxtemp, int mov, int ext, int fan);
+    double currTemp;
+    void setMaxTemp(int t);
+    void setMovSpeed(int s);
+    void setExtrusionSpeed(int s);
+    void setFanSpeed(int s);
+    int getMaxTemp();
+    int getMovSpeed();
+    int getExtrusionSpeed();
+    int getFanSpeed();
+
+private:
+    int maxTemp;
+    int movimentSpeed;
+    int extrusionSpeed;
+    int fanSpeed;
+    int extruderNumber;
 };
 
 #endif // EXTRUDEROBJECT_H
