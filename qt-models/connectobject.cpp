0833162428694ffa5a864ea93f115f591c574043
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-03-25 13:15:29 -0300

    Add data in this class
    -Add the variables
    -Add the get/set methods to manipulate the data
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- qt-models/connectobject.cpp
+++ qt-models/connectobject.cpp
@@ -4,3 +4,53 @@ ConnectObject::ConnectObject()
 {
 
 }
+
+void ConnectObject::setSerialPort(QString s)
+{
+    serialPort = s;
+}
+
+void ConnectObject::setTransmissionRate(int r)
+{
+    transmissionRate = r;
+}
+
+void ConnectObject::setCacheSize(int s)
+{
+    cacheSize = s;
+}
+
+void ConnectObject::setResetOnConnect(bool b)
+{
+    resetOnConnect = b;
+}
+
+void ConnectObject::setPrintLog(bool b)
+{
+    printLog = b;
+}
+
+QString ConnectObject::getSerialPort()
+{
+    return serialPort;
+}
+
+int ConnectObject::getTransmissionRate()
+{
+    return transmissionRate;
+}
+
+int ConnectObject::getCacheSize()
+{
+    return cacheSize;
+}
+
+bool ConnectObject::getResetOnConnect()
+{
+    return resetOnConnect;
+}
+
+bool ConnectObject::getPrintLog()
+{
+    return printLog;
+}
--- qt-models/connectobject.h
+++ qt-models/connectobject.h
@@ -1,11 +1,29 @@
 #ifndef CONNECTOBJECT_H
 #define CONNECTOBJECT_H
-
+//Qt Includes
+#include <QtCore/QString>
 
 class ConnectObject
 {
 public:
     ConnectObject();
+    void setSerialPort(QString s);
+    void setTransmissionRate(int r);
+    void setCacheSize(int s);
+    void setResetOnConnect(bool b);
+    void setPrintLog(bool b);
+    QString getSerialPort();
+    int getTransmissionRate();
+    int getCacheSize();
+    bool getResetOnConnect();
+    bool getPrintLog();
+
+private:
+    QString serialPort;
+    int transmissionRate;
+    int cacheSize;
+    bool resetOnConnect;
+    bool printLog;
 };
 
 #endif // CONNECTOBJECT_H
