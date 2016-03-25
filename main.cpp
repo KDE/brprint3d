df1125b3acdb6a7db29b0d58a95d09bde0e5fc03
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-03-25 13:21:58 -0300

    Add the name of organization, domain and name of the application
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- main.cpp
+++ main.cpp
@@ -4,5 +4,11 @@ int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
 
+    QCoreApplication::setOrganizationName("KDE");
+
+    QCoreApplication::setOrganizationDomain("kde.org");
+
+    QCoreApplication::setApplicationName("brprint3d");
+
     return app.exec();
 }
