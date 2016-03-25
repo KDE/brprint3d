76b0ee9f0110dfb6aeca9489031168592392ff46
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-03-25 13:16:58 -0300

    Override methods
    Add the override methods to manipulate the data
    from connectobject
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- qt-models/connectionmodel.cpp
+++ qt-models/connectionmodel.cpp
@@ -1,6 +1,42 @@
 #include "connectionmodel.h"
 
-connectionModel::connectionModel()
+connectionModel::connectionModel(ConnectObject *c)
 {
+    connect = c;
+}
 
+int connectionModel::rowCount(const QModelIndex &parent) const
+{   Q_UNUSED(parent);
+    return 5;
+}
+
+QVariant connectionModel::data(const QModelIndex &index, int role) const
+{
+        if(Qt::DisplayRole != role){
+                return QVariant();
+        }
+        else{
+            switch (index->row()){
+            case 0: return connect->getSerialPort();
+            case 1: return connect->getTransmissionRate();
+            case 2: return connect->getCacheSize();
+            case 3: return connect->getResetOnConnect();
+            case 4: return connect->getPrintLog();
+            }
+        }
+}
+
+bool connectionModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+    if(role == Qt::EditRole){
+        switch (index.row()) {
+            case 0: connect->setSerialPort(value.toString()); emit dataChanged(index,index);
+            case 1: connect->setTransmissionRate(value.toInt()); emit dataChanged(index,index);
+            case 2: connect->setCacheSize(value.toInt()); emit dataChanged(index,index);
+            case 3: connect->setResetOnConnect(value.toBool()); emit dataChanged(index,index);
+            case 4: connect->setPrintLog(value.toBool()); emit dataChanged(index,index);
+        }
+        return true;
+    }
+    return false;
 }
--- qt-models/connectionmodel.h
+++ qt-models/connectionmodel.h
@@ -1,11 +1,23 @@
 #ifndef CONNECTIONMODEL_H
 #define CONNECTIONMODEL_H
+//QtIncludes
+#include <QtCore/QAbstractListModel>
 
+//Local Includes
+#include "connectobject.h"
 
 class connectionModel : public QAbstractListClass
 {
 public:
-    connectionModel();
+    connectionModel(ConnectObject *c);
+
+private:
+    //Override Methods
+    virtual int rowCount(const QModelIndex &parent) const;
+    virtual QVariant data(const QModelIndex &index, int role) const;
+    virtual bool setData(const QModelIndex &index, const QVariant &value, int role);
+    virtual Qt::ItemFlags flags(const QModelIndex &index) const;
+    ConnectObject *connect;
 };
 
 #endif // CONNECTIONMODEL_H
