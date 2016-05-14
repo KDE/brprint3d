11b781e048378f7bc3735389ec6974633642cfca
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-05-14 01:24:40 -0300

    Create a class to manage gcode highlighter
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

---
+++ gcodehighlighter.cpp
@@ -0,0 +1,6 @@
+#include "gcodehighlighter.h"
+
+GCodeHighLighter::GCodeHighLighter()
+{
+
+}
---
+++ gcodehighlighter.h
@@ -0,0 +1,11 @@
+#ifndef GCODEHIGHLIGHTER_H
+#define GCODEHIGHLIGHTER_H
+
+
+class GCodeHighLighter : public QSyntaxHighlighter
+{
+public:
+    GCodeHighLighter();
+};
+
+#endif // GCODEHIGHLIGHTER_H
