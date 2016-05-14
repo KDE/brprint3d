aafb00339fd6107690ce63eeca0d7a03f1df0b87
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-05-14 01:28:04 -0300

    Add features
    Add the default overrited function
    Create the patterns for syntaxhighlighters
    Create the colors patterns for highlight
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- gcodehighlighter.cpp
+++ gcodehighlighter.cpp
@@ -1,6 +1,66 @@
 #include "gcodehighlighter.h"
 
-GCodeHighLighter::GCodeHighLighter()
+GCodeHighLighter::GCodeHighLighter(QTextDocument *parent): QSyntaxHighlighter(parent)
 {
+    setForegrounds();
+    setRules();
+}
 
+void GCodeHighLighter::highlightBlock(const QString &text)
+{
+    foreach (const HighlightingRule &rule, highlightingRules) {
+        QRegExp expression(rule.pattern);
+        int index = expression.indexIn(text);
+        while (index >= 0) {
+            int length = expression.matchedLength();
+            setFormat(index, length, rule.format);
+            index = expression.indexIn(text, index + length);
+        }
+    }
+    //I'm not sure what this do... copy from example
+    //http://doc.qt.io/qt-5/qtwidgets-richtext-syntaxhighlighter-highlighter-cpp.html
+    setCurrentBlockState(0);
+}
+
+void GCodeHighLighter::setRules()
+{
+    HighlightingRule rule;
+    /*M + value*/
+    rule.format = mCommands;
+    rule.pattern = QRegExp("\\bM(\\d+)\\b");
+    highlightingRules.append(rule);
+    /*G + value*/
+    rule.format = gCommands;
+    rule.pattern = QRegExp("\\bG(\\d+)\\b");
+    highlightingRules.append(rule);
+    /*X | Y | Z*/
+    rule.format = xyzCommands;
+    rule.pattern = QRegExp("\\bX|Y|Z\\b");
+    highlightingRules.append(rule);
+    /*F*/
+    rule.format = fCommands;
+    rule.pattern = QRegExp("\\bF.+\\b");
+    highlightingRules.append(rule);
+    /*E*/
+    rule.format = eCommands;
+    rule.pattern = QRegExp("\\bE.+\\b");
+    highlightingRules.append(rule);
+    /*comments*/
+    rule.format = comments;
+    rule.pattern = QRegExp("\\b;.+\\b");
+    highlightingRules.append(rule);
+    /*Numbers*/
+    rule.format = numbers;
+    rule.pattern = QRegExp("\\(\\d+.\\d+)\\b");
+    highlightingRules.append(rule);
+}
+
+void GCodeHighLighter::setForegrounds()
+{
+    gCommands.setForeground(Qt::darkCyan);
+    mCommands.setForeground(Qt::magenta);
+    fCommands.setForeground(Qt::yellow);
+    eCommands.setForeground(Qt::red);
+    comments.setForeground(Qt::darkGreen);
+    numbers.setForeground(Qt::darkRed);
 }
--- gcodehighlighter.h
+++ gcodehighlighter.h
@@ -1,11 +1,34 @@
 #ifndef GCODEHIGHLIGHTER_H
 #define GCODEHIGHLIGHTER_H
+#include <QSyntaxHighlighter>
 
 
 class GCodeHighLighter : public QSyntaxHighlighter
 {
 public:
-    GCodeHighLighter();
+    GCodeHighLighter(QTextDocument *parent);
+
+protected:
+    void highlightBlock(const QString &text) Q_DECL_OVERRIDE;
+
+private:
+    struct HighlightingRule
+    {
+            QRegExp pattern;
+            QTextCharFormat format;
+    };
+
+    QList <HighlightingRule> highlightingRules;
+    QTextCharFormat gCommands;
+    QTextCharFormat mCommands;
+    QTextCharFormat fCommands;
+    QTextCharFormat eCommands;
+    QTextCharFormat comments;
+    QTextCharFormat numbers;
+    QTextCharFormat xyzCommands;
+    void setForegrounds();
+    void setRules();
+
 };
 
 #endif // GCODEHIGHLIGHTER_H
