4ae586ce76f30676ba8f2ad47494a7ca04cd21f3
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-05-15 10:59:13 -0300

    New methods
    Define the methods to handle the gcode data.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- gcodehandler.cpp
+++ gcodehandler.cpp
@@ -1,6 +1,62 @@
 #include "gcodehandler.h"
+#include <QTextStream>
+#include <QFileInfo>
 
 GCodeHandler::GCodeHandler(QObject *parent) : QObject(parent)
 {
 
+}
+
+/*
+ * Save edit file - overrite
+*/
+void GCodeHandler::saveFile() //Default pattern .gcode - Needs handle it?
+{
+    QFile gcode(m_filePath.toLocalFile());
+    if(!gcode.open(QFile::WriteOnly | QFile::Text)){
+        gcode.write(m_doc->toPlainText().toUtf8());
+        gcode.close();
+    }
+
+}
+
+void GCodeHandler::setFilePath(QUrl &name)
+{
+    if(m_filePath != name){
+        m_filePath = name;
+        QString path = name.toLocalFile();
+        setFileName(QFileInfo(path).baseName());
+        setFileContent(path);
+    }
+}
+
+QString GCodeHandler::fileName() const
+{
+    return m_fileName;
+}
+
+QString GCodeHandler::fileContent() const
+{
+    return m_fileContent;
+}
+
+void GCodeHandler::setFileContent(QString path)
+{
+    if (!path.isEmpty()) { //Remove this test???
+        QFile gcode(path);
+        if (!gcode.open(QFile::ReadOnly | QFile::Text)) {
+            return;
+        }
+        QTextStream in(&gcode);
+        m_fileContent = in.readAll();
+        emit fileContentChanged(m_fileContent);
+    }
+}
+
+void GCodeHandler::setFileName(QString n)
+{
+    if(m_fileName != n){
+        m_fileName = n;
+        emit fileNameChanged(m_fileName);
+    }
 }
--- gcodehandler.h
+++ gcodehandler.h
@@ -1,16 +1,36 @@
 #ifndef GCODEHANDLER_H
 #define GCODEHANDLER_H
-
+//QtIncludes
+#include <QString>
+#include <QQuickTextDocument>
+#include <qqmlfile.h>
+#include <QTextCodec>
 
 class GCodeHandler : public QObject
 {
     Q_OBJECT
+
+    Q_PROPERTY (QString fileName READ fileName WRITE setFileName NOTIFY fileNameChanged)
+    Q_PROPERTY (QString fileContent READ fileContent WRITE setFileContent NOTIFY fileContentChanged)
+    Q_INVOKABLE void saveFile();
+
 public:
     explicit GCodeHandler(QObject *parent = 0);
+    QString fileName() const;
+    QString fileContent() const;
+    void setFileContent(QString path);
+    void setFileName(QString n);
+    void setFilePath(QUrl &name);
+
+private:
+    QString m_fileName;
+    QString m_fileContent;
+    QUrl m_filePath;
+    QTextDocument *m_doc;
 
 signals:
-
-public slots:
+    void fileContentChanged(const QString &content);
+    void fileNameChanged(const QString &name);
 };
 
 #endif // GCODEHANDLER_H
